9e2ef2fb6b8c6f6c1f1a16f0fa9bf2f6b0401097
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2020-11-20 14:36:28 +0100

    Préparation des fichiers à rendre V2

--- memoire/malloc_stub.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MALLOC_STUB_H__
-#define __MALLOC_STUB_H__
-#include <stdlib.h>
-void *malloc(size_t s);
-void *calloc(size_t count, size_t size);
-void *realloc(void *ptr, size_t size);
-void free(void *ptr);
-#endif
